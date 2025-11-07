35b06d093a504d91ccc3c84225eb2d0683608bcb
rasgaxota572 <rasgaxota575@gmail.com>
2025-11-07 14:24:50 -0300

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Option on whether to upload the produced build to a file hosting service [Useful for CI builds]
+UPLD=1
+	if [ $UPLD = 1 ]; then
+		UPLD_PROV="https://oshi.at"
+        UPLD_PROV2="https://transfer.sh"
+	fi
+
+sudo apt-get install ccache
+
+# Clone toolchain from its repository
+mkdir clang && curl -Lsq https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-release/clang-r383902.tar.gz -o - | tar -xzf - -C clang
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android11-release binutils
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android11-release binutils-32
+
+# Clone zip
+git clone --depth=1https://github.com/rasgaxota572/zip -b sunburn-$1
+
+# Export the PATH variable
+export PATH="$(pwd)/clang/bin:$(pwd)/binutils/bin:$(pwd)/binutils-32/bin:$PATH"
+
+# Clean up out
+find out -delete
+mkdir out
+
+# Compile the kernel
+build_clang() {
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=arm64 \
+    CC=clang \
+    HOSTCC=clang \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+}
+
+make exynos9830-$1_defconfig ARCH=arm64 O=out CC=clang
+build_clang
+
+else
+    echo "The kernel has failed to compile. Please check the terminal output for further details."
+    exit 1
+fi
